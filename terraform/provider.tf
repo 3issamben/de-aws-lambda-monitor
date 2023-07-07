a9cfae50d439300b1346859f6d524e49102b44fd
Issam Ben <i.bensirhir@digitaledge.net>
2023-07-07 16:43:18 +0200

    chore: add default tags

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,10 @@
 provider "aws" {
   region = var.region
-
+  default_tags {
+    tags = {
+      Environment = "test"
+      Owner       = "issam"
+      Application = "de-aws-monitor"
+    }
+  }
 }
