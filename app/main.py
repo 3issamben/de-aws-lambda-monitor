2e696b44a260656194ee5f45ae5ab6c6857eb662
Issam Ben <i.bensirhir@digitaledge.net>
2023-07-28 19:26:43 +0200

    feat: update lambda handler

--- app/main.py
+++ app/main.py
@@ -1,5 +1,29 @@
-from main_test import test_selenium
+from task_manager import main
+import os
+from logger_config import logger
+import json
+
+
+# Settings
 
 
 def lambda_handler(event=None, context=None):
-    test_selenium()
+    logger.info("started lambda function")
+    try:
+        puller_id = event.get("puller_id") or os.environ.get(
+            "PULLER_ID", "11"
+        )
+
+        result = main(puller_id)
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"result": result}),
+        }
+
+    except Exception as e:
+        logger.error(f"Error occurred: {e}")
+        return {
+            "statusCode": 500,
+            "body": json.dumps({"error": "An error occurred"}),
+        }
